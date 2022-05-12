e01204035259c01cb7166ef368df62579004bcdd
ViswaSai13 <viswasaitejak@gmail.com>
2022-05-12 23:57:47 +0530

    chart components setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mock-d3-charts';
+  isExpand = false
+  toggleExpand(){
+    this.isExpand = !this.isExpand
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineChartComponent,
+    PieChartComponent,
+    BarChartComponent,
+    BubbleChartComponent
   ],
   imports: [
     BrowserModule
